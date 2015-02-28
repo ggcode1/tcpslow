94ee013fc63ec1061300885a51b7abe1dde4b909
llambda <xxgsoftware@gmail.com>
2015-02-28 13:58:07 -0600

    Bug fixes

--- tcpslow.js
+++ tcpslow.js
@@ -1,84 +1,140 @@
 #!/usr/bin/env node
+'use strict';
 
 var program = require('commander');
-var package = require('./package.json');
+var packagejson = require('./package.json');
 
 program
-.version(package.version)
+.version(packagejson.version)
 .description('Delay packets')
 .option('-l, --listen [port]', 'TCP port to listen on', parseInt)
+.option('-L, --listenuds [filename]', 'Unix domain socket to listen on')
 .option('-f, --forward [port]', 'TCP port to forward to', parseInt)
-.option('-L, --listenuds [filename]', 'Unix domain socket to listen to')
 .option('-F, --forwarduds [filename]', 'Unix domain socket to forward to')
-.option('-d, --delay [ms]', 'Milliseconds to delay', parseInt)
-.option('-h, --host [hostname]', 'Hostname of remote')
+.option('-d, --delay [ms]', 'Optional milliseconds to delay', parseInt)
+.option('-h, --host [hostname]', 'Hostname of remote. Cannot be used with -F')
 .option('-v, --verbose', 'Log connection events')
 .option('-p, --packet', 'Log data transmitted')
-.option('-s, --sending', 'delay packet sending in addition to receiving', false)
+.option('-s, --sending', 'Delay packet sending in addition to receiving', false)
 .parse(process.argv);
 
-// if (!(program.listen || program.listenuds)) {
-//   program.help();
-// }
+if (program.listen && program.listenuds) {
+  console.error('Can\'t listen on a TCP and a Unix domain socket');
+  program.help();
+}
 
-// if (!(program.forward || program.forwarduds)) {
-//   program.help();
-// }
+if (program.forward && program.forwarduds) {
+  console.error('Can\'t forward to a TCP port and a Unix domain socket');
+  program.help();
+}
+
+if (program.host && program.forwarduds) {
+  console.error('Can\'t use a hostname with a forwarding Unix domain socket');
+  program.help();
+}
+
+if (!(program.listen || program.listenuds)) {
+  console.error('Need a listening port');
+  program.help();
+}
+
+if (!(program.forward || program.forwarduds)) {
+  console.error('Need a forwarding port');
+  program.help();
+}
+
+// normalize path strings
+if (program.forwarduds) {
+  program.forwarduds = path.normalize(program.forwarduds);
+}
+
+if (program.listenuds) {
+  program.listenuds = path.normalize(program.listenuds);
+}
 
 var net = require('net');
 var chalk = require('chalk');
+var path = require('path');
 
+function createConnection() {
+  var conn;
 
-var conn = {};
-conn.port = program.forward;
-if (program.host)
-  conn.host = program.host;
+  if (program.host || program.forward) {
+    conn = {};
+    conn.port = program.forward;
+    if (program.host) {
+      conn.host = program.host;
+    } else {
+      conn.host = 'localhost';
+    }
+    return net.createConnection(conn);
+  } else {
+    return net.createConnection(path.normalize(program.forwarduds));
+  }
+}
 
 var server = net.createServer(function(listen) {
-  var forward;
+  if (program.verbose) console.log(new Date() + ' (listening) client connected ');
 
-  forward = net.createConnection(conn);
-  forward.on('connect', function() {
-    if (program.verbose) console.log(new Date() + ' Client connected.');
+  listen.forward = createConnection();
+  listen.forward.on('connect', function() {
+    if (program.verbose) console.log(new Date() + ' (forwarding) client connected.');
   });
-  forward.on('data', function(data) {
+  listen.forward.on('data', function(data) {
     if (program.packet) console.log(chalk.red(data))
-    setTimeout(function() {
-      listen.write(data);
-    }, program.delay);
+      setTimeout(function() {
+        listen.write(data);
+      }, program.delay);
   });
-  forward.on('error', function(err) {
-    console.error(err);
+  listen.forward.on('error', function(err) {
+    if (program.verbose) console.log(new Date() + ' (forwarding) error ' + err);
+    listen.destroy();
   });
-  forward.on('end', function() {
-    if (program.verbose) console.log(new Date() + ' Client disconnected.');
+  listen.forward.on('end', function() {
+    if (program.verbose) console.log(new Date() + ' (forwarding) client end.');
+    listen.end();
   });
+  listen.forward.on('close', function() {
+    if (program.verbose) console.log(new Date() + ' (forwarding) client close.');
+    listen.end();
+  })
 
   listen.on('data', function(data) {
     if (program.sending) {
       setTimeout(function() {
-        forward.write(data);
+        listen.forward.write(data);
       }, program.delay);
     } else {
-      forward.write(data);
+      listen.forward.write(data);
     }
     if (program.packet) console.log(chalk.blue(data));
   });
   listen.on('end', function() {
-    if (log) console.log(new Date() + ' Socket end.');
-    forward.end();
+    if (program.verbose) console.log(new Date() + ' (listening) socket end.');
+    listen.forward.end();
     listen.end();
   });
   listen.on('error', function(err) {
-    if (program.verbose) console.log(new Date() + ' ' + err);
+    if (program.verbose) console.log(new Date() + ' (listening) error: ' + err);
+    listen.forward.destroy();
   });
-
+  listen.on('close', function() {
+    var args = Array.prototype.slice.call(arguments);
+    if (program.verbose) console.log(new Date() + ' (listening) close: ' + args);
+    listen.forward.end();
+  });
 });
 
-server.listen(program.listen, function() {
-  console.log('tcpslow listening on port ' + program.listen + ' relaying to' +
-    (program.host ? +' ' + program.host : '') + ' port ' + program.forward +
-    ' delaying by ' +
-    program.delay +
-    'ms' + (program.sending ? ' in both directions ' : ' on receive'));
+server.listen(program.listen ? program.listen : program.listenuds, function() {
+  console.log('tcpslow ' + packagejson.version);
+  if (program.listen) {
+    console.log('Listening on port ' + program.listen);
+  } else {
+    console.log('Listening on unix domain socket ' + program.listenuds);
+  }
+  console.log('Relaying to ' + (program.host ? program.host + ' ' : '') 
+    + 'port ' + program.forward);
+  if (program.delay) {
+    console.log(' delaying by ' + program.delay + 'ms' + (program.sending ? ' in both directions ' : ' on receive'));
+  }
 });
